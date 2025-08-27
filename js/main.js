afb1f9122f8c0024e677cc8a4f51a8da40bcf19b
Khrishman Khadka <mr.khadka99@gmail.com>
2025-08-27 22:48:33 +0545

    Close Mobile Menu

--- js/main.js
+++ js/main.js
@@ -137,3 +137,14 @@ function toggleMobileMenu() {
   hamburger.classList.toggle("active");
   navLinks.classList.toggle("active");
 }
+
+// Close Mobile Menu
+function closeMobileMenu() {
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("navLinks");
+
+  if (hamburger && navLinks) {
+    hamburger.classList.remove("active");
+    navLinks.classList.remove("active");
+  }
+}
